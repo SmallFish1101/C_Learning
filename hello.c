60b09d882327b31d875df2c02e714bf4e059620b
SmallFish1101 <3090807415@qq.com>
2026-07-02 15:47:18 +0800

    添加编译时间戳输出

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(void)
 {
-    printf("Hello, World!\n");
- 
+    printf("Hello, Electronic Engineering!\n");
+    printf("Built on:%s %s\n", __DATE__, __TIME__);
     return 0;
 }
