b9ac0b63d244559db2dceda142e74c0e0c6b1743
SmallFish1101 <3090807415@qq.com>
2026-07-05 18:35:36 +0800

    在 feature/tiom 分支上添加日期输出

--- hello.c
+++ hello.c
@@ -3,6 +3,6 @@ int main(void)
 {
     printf("Hello, Electronic Engineering!\n");
     printf("Built on:%s %s\n", __DATE__, __TIME__);
-    printf("This is a new feature branch!\n");
+    printf("Today is: %s %s\n", __DATE__, __TIME__);
     return 0;
 }
